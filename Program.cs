b438016a3a9faa018b8181b54215f398d553bdf9
RafaMufid <akurafamufid@gmail.com>
2025-03-10 10:03:48 +0700

    menambahkan implementasi dengan table-driven

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class KodePos
+{
+    public static void getKodePos(string daerah)
+    {
+        string[,] dataKodePos =
+        {
+            {"Batununggal", "40266" },
+            {"Kujangsari", "40287" },
+            {"Mengger", "40267" },
+            {"Wates", "40256" },
+            {"Cijaura", "40287" },
+            {"Jatisari", "40286" },
+            {"Margasari", "40286" },
+            {"Sekejati", "40286" },
+            {"Kebonwaru", "40272" },
+            {"Maleer", "40274" },
+            {"Samoja", "40273" },
+        };
+    }
+}
